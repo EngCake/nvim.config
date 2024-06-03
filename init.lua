c927c67a4506ce75d8ac3f7324eb3b61f33855d3
KhalidNusserat <KhMKhNu@gmail.com>
2024-06-03 21:33:06 +0300

    refactor nvim-tree and make it centered

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
